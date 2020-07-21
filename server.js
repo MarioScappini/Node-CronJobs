513e1a572725f6801935a6f57d66f3b63a30ab48
Mario Scappini <mario.scappiniu@gmail.com>
2020-07-21 12:19:38 -0400

    installing dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var schedule = require('node-schedule');
+
+app = express();
+
+var counter = 0;
+let uniqueJobName = 'test';
+var job = schedule.scheduleJob(uniqueJobName,'*/10 * * * * *', function(){
+    console.log('The counter is'+counter++);
+    test();
+});
+
+function test(){
+    if (counter>2) {
+        let current_job = schedule.scheduledJobs[uniqueJobName];
+        current_job.cancel();
+        console.log('done');
+    }
+}
+
+
+
+app.listen(3128);
